f7e3a040943c710d366f2c68ed171d2924bddec1
spadino <spada.andrea@gmail.com>
2021-08-21 19:42:01 -0300

    add tasks/auto-update-submodules.sh

---
+++ tasks/auto-update-submodules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Arjan Scherpenisse, 2012-05-16
+#
+# Automatically update and commit a parent repository when a submodule changed
+
+DIR=$(dirname $PWD)
+PART=$(basename $PWD)
+cd ..
+
+while [ "$DIR" != "/" ]; do
+    PARENTREPO=$(basename $DIR)
+
+    if [ -d "$DIR/.git" ]; then
+        if $(git diff $PART|grep "dirty" >/dev/null); then
+            echo "This repository is dirty, cannot updated parent repository $PARENTREPO."; 
+            exit 1
+        fi
+        git add $PART
+        git commit -m "Updated submodule `basename $PART`"
+        git push
+        echo "Commited and pushed to $PARENTREPO."
+        break
+    fi
+    PART="`basename $DIR`/$PART"
+    DIR=$(dirname $DIR)
+    cd ..
+done
